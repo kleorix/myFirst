f1c862e10cd71d969b4a6916763da8b83671c9fa
unknown <0912144@gmail.com>
2013-05-29 22:19:20 +0700

    Initialize Res

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyFirst::Application.config.secret_token = '140524b45946987e790b7417f05cd9fd11434f50e3bb5e59c6b21b107e93ec8f1eb385f26e257a848312c4b85e528537ea346082ec88be3839485c8f5a714001'
